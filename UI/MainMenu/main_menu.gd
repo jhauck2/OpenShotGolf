58ba046e8a19cadc29d44f4555a7dcd9b5cee73e
Jesse Hernandez <futcomker@gmail.com>
2026-02-27 02:34:03 -0800

    feat: changing the main menu

--- UI/MainMenu/main_menu.gd
+++ UI/MainMenu/main_menu.gd
@@ -1,11 +1,32 @@
 extends Control
 
+@onready var _settings_button: Button = $TopBanner/LeftButtons/SettingsButton
+@onready var _exit_button: Button = $TopBanner/LeftButtons/ExitButton
+@onready var _courses_button: Button = $TilesRow/CoursesTile/CoursesTextBackdrop/CoursesButton
+@onready var _range_button: Button = $TilesRow/RangeTile/RangeTextBackdrop/RangeButton
+@onready var _version_label: Label = $VersionLabel
+var _version_fall_back: String = "dev"
+var _version_setting_path: String = "application/config/version"
+var _version_text: String
+
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+func _ready():
+	if _exit_button != null:
+		_exit_button.pressed.connect(_on_exit_pressed)
+		
+	if _settings_button != null:
+		_settings_button.pressed.connect(_on_settings_pressed)
+		
+	if _range_button != null:
+		_range_button.pressed.connect(_on_range_pressed)
+	if _courses_button != null:
+		_courses_button.pressed.connect(_on_courses_pressed)
+		
+	_update_version_label()
 	SceneManager.current_scene = self
 
-
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
@@ -19,6 +40,16 @@ func _on_courses_pressed() -> void:
 	SceneManager.change_scene("res://Courses/CourseSelector/course_selector.tscn")
 
 
+func _update_version_label():
+	_version_text = _version_fall_back
+	if (ProjectSettings.has_setting(_version_setting_path)):
+		var _configured_version = str(ProjectSettings.get_setting(_version_setting_path)).strip_edges()
+		_version_text = _configured_version;
+
+		
+	if _version_label != null:	
+		_version_label.text = "Version %s" % _version_text
+	
 func _on_settings_pressed() -> void:
 	pass # Replace with function body.
 
