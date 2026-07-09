1256d4c75e95cac9dfed890b70854bb4acd4d2f4
Jake <jghauck17@gmail.com>
2026-07-08 20:44:31 -0500

    Converted cd_hi, cl_hi, cd_low tables to hex ball values

--- Physics/LookupTables/cd_data_hi_re.gd
+++ Physics/LookupTables/cd_data_hi_re.gd
@@ -3,4 +3,4 @@ extends Resource
 # each collumn corresponds to the following spin values
 var spinValues : Array[float] = [0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.10, 0.11, 0.12, 0.13, 0.20, 0.30]
 
-var data : Array[float] = [0.264, 0.263, 0.262, 0.262, 0.263, 0.264, 0.265, 0.266, 0.268, 0.274, 0.280, 0.283, 0.299, 0.328]
+var data : Array[float] = [0.247, 0.246, 0.245, 0.245, 0.246, 0.247, 0.249, 0.251, 0.254, 0.262, 0.270, 0.275, 0.292, 0.338]
