8059d9ba28641b54434fe618092cea90a4459870
Jake <jghauck17@gmail.com>
2026-08-13 18:16:42 -0500

    Small main menu tweaks

--- UI/MainMenu/new_main_menu.gd
+++ UI/MainMenu/new_main_menu.gd
@@ -3,9 +3,13 @@ extends ColorRect
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$VBoxContainer/Version.text = "Version " + GlobalSettings.get_version_string()
+	$VBoxContainer/HBoxContainer/Version.text = "Version " + GlobalSettings.get_version_string()
 	SceneManager.current_scene = self
 
 
 func _on_range_button_pressed() -> void:
 	SceneManager.change_scene("res://Courses/Range/range.tscn")
+
+
+func _on_physics_test_button_pressed() -> void:
+	PhysicsTest.runTests()
